f1a1a041a0a373905a4ae4c8e267dec9590b8306
Zuzia Piotrowska <zuzannapiotrowska2002@gmail.com>
2020-06-13 00:02:33 +0200

    I made a mess, kinda works??

--- script.js
+++ script.js
@@ -23,18 +23,27 @@ function createBoard() {
     "grid-template-columns: repeat(" + numberOfRows + ", 1fr)"
   );
   for (i = 0; i < numberOfRows * numberOfRows; i++) {
-
     let square = document.createElement("div");
     container.appendChild(square);
     square.classList.add("square");
   }
 
   document.querySelectorAll(".square").forEach((item) => {
-    item.addEventListener("mouseover", (event) => {
-      item.style.cssText = "background-color: black";
-    });
+    drawBlack(item);
   });
 }
+function drawBlack(item) {
+  item.addEventListener("mouseover", (event) => {
+    item.style.backgroundColor = "black";
+  });
+}
+const blackBtn = document.querySelector(".blackBtn");
+
+blackBtn.addEventListener("click", function (e) {
+  document.querySelectorAll(".square").forEach((item) => {
+    drawBlack(item);
+  });
+});
 
 const resetBtn = document.querySelector(".resetBtn");
 
@@ -44,3 +53,41 @@ resetBtn.addEventListener("click", function (e) {
   });
   createBoard();
 });
+
+const colorBtn = document.querySelector(".colorBtn");
+
+colorBtn.addEventListener("click", function (e) {
+  color();
+});
+function color() {
+  document.querySelectorAll(".square").forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      let min = 1;
+      let max = 360;
+
+      let hue = Math.floor(Math.random() * (max - min + 1)) + min;
+      item.style.backgroundColor = "hsl(" + hue + ", 90%, 70%)";
+    });
+  });
+}
+
+const shadeBtn = document.querySelector(".shadeBtn");
+
+shadeBtn.addEventListener("click", function (e) {
+  shade();
+});
+function shade() {
+  document.querySelectorAll(".square").forEach((item) => {
+    item.addEventListener("mouseover", (event) => {
+      if (!item.style.opacity) {
+        item.style.opacity = 0;
+      }
+
+      item.style.backgroundColor = "black";
+
+      if (item.style.opacity < 1) {
+        item.style.opacity = Number(item.style.opacity) + 0.1;
+      }
+    });
+  });
+}
